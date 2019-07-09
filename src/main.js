86139971c8450590aad2ca0888e05e573acc5996
jsmask <2674800533@qq.com>
2019-07-09 10:43:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -10,10 +10,14 @@ require("./css/normal.css");
 import Vue from 'vue';
 import App from './App.vue'
 
+// new Vue({
+//     el: "#app",
+//     template: "<App />",
+//     components: {
+//         App
+//     }
+// })
+
 new Vue({
-    el: "#app",
-    template: "<App />",
-    components: {
-        App
-    }
-})
+    render: h => h(App)
+}).$mount("#app");
