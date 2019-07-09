7ff7db591c4300dc064692bec95664ea5a27c4fb
jsmask <2674800533@qq.com>
2019-07-09 15:14:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 require("./css/normal.css");
 
-// const sum = require("./js/sum");
-
-// let a = 10;
-// let b = 20;
-
-// console.log(sum(a, b));
-
 import Vue from 'vue';
 import App from './App.vue'
 
